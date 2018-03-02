184d4a0a6bf6364ea383f0fe03505b799c528172
JudahRR <33609142+JudahRR@users.noreply.github.com>
2018-03-02 12:07:21 -0500

    Create say.js

---
+++ libs/say.js
@@ -0,0 +1,1 @@
+function say(m){ 	var msg = new SpeechSynthesisUtterance(); 	var voices = window.speechSynthesis.getVoices(); 	msg.voice = voices[10]; 	msg.voiceURI = "native"; 	msg.volume = 1; 	msg.rate = 1; 	msg.pitch = 0.8; 	msg.text = m; 	msg.lang = 'en-US';   	speechSynthesis.speak(msg); }
